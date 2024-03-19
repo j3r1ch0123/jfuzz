cd0da73e62039d2a58b0f19817d3d2458fae7a05
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2024-03-19 14:12:48 -0400

    Update jfuzz.py
    Now uses threads

--- jfuzz.py
+++ jfuzz.py
@@ -2,6 +2,7 @@
 import requests
 import sys
 import getopt
+import threading
 
 banner = """\
      ██╗███████╗██╗   ██╗███████╗███████╗
@@ -13,32 +14,46 @@ banner = """\
                                                                                          
 """
 
-def main(argv):
+def run(url, wordlist, thread_number):
+    threads = []
+    for i in range(int(thread_number)):
+        thread = threading.Thread(target=main, args=(url, wordlist))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+def main(url, wordlist):
+    for line in wordlist:
+        fuzz = url + line.strip()
+        response = requests.get(fuzz)
+        print(fuzz, response.status_code)
+
+if __name__ == "__main__":
+    print(banner)
     url = ''
     wordlist = ''
+    thread_number = 5
 
     try:
-        opts, args = getopt.getopt(argv, "hu:w:", ["url=", "wordlist="])
-
+        opts, args = getopt.getopt(sys.argv[1:], "hu:w:t:", ["url=", "wordlist=", "threads="])
     except getopt.GetoptError:
-        print("Format: python3 jfuzz.py -u <url> -w <wordlist>")
+        print("Format: python3 jfuzz.py -u <url> -w <wordlist> -t <threads>")
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == "-h":
-            print("Format: python3 jfuzz.py -u <url> -w <wordlist>")
+            print("Format: python3 jfuzz.py -u <url> -w <wordlist> -t <threads>")
             sys.exit()
         elif opt in ("-u", "--url"):
             url = arg
         elif opt in ("-w", "--wordlist"):
             wordlist = arg
+        elif opt in ("-t", "--threads"):
+            thread_number = int(arg)
 
     with open(wordlist, "r") as words:
-        for line in words.readlines():
-            fuzz = url + line.strip()
-            response = requests.get(fuzz)
-            print(fuzz, response.status_code)
+        wordlist_lines = words.readlines()
 
-if __name__ == "__main__":
-    print(banner)
-    main(sys.argv[1:])
+    run(url, wordlist_lines, thread_number)
